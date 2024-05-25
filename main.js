ac8749315bf6dce71d5fcb9f67ca40599415656e
@Zam <141449338+AzamiJs@users.noreply.github.com>
2024-05-24 21:40:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -574,7 +574,8 @@ user.afkReason = ''
 //ARRANCA LA DIVERSIÓN  
 switch (prefix && command) { 
 case 'test': {
-m.reply('a')
+const testI = generateWAMessageFromContent(from, { viewOnceMessage: { message: { "messageContextInfo": { "deviceListMetadata": {}, "deviceListMetadataVersion": 2 }, interactiveMessage: proto.Message.InteractiveMessage.create({ body: proto.Message.InteractiveMessage.Body.create({ text: '' }), footer: proto.Message.InteractiveMessage.Footer.create({ text: 'NovaBot' }), header: proto.Message.InteractiveMessage.Header.create({ title: 'Nova', subtitle: 'MarioJs', hasMediaAttachment: false }), nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({ buttons: [ { "name": "quick_reply", "buttonParamsJson": `{"display_text":"Menu","id":".menu"}` }, { "name": "quick_reply", "buttonParamsJson": `{"display_text":"Ping","id":".ping"}` }], })})}}}, {})
+return conn.relayMessage(testI.key.remoteJid, testI.message, { messageId: testI.key.id }, {quoted: m})
 }
 break
 
